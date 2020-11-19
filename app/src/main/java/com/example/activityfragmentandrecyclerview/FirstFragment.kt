114408de60b864e648f2a5cec67c63a96db255d5
A_Anes <abdic.anes@gmail.com>
2020-11-19 13:15:07 +0100

    Tried to send user from one fragment to the other, app crashes

--- app/src/main/java/com/example/activityfragmentandrecyclerview/FirstFragment.kt
+++ app/src/main/java/com/example/activityfragmentandrecyclerview/FirstFragment.kt
@@ -4,27 +4,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
-import com.example.activityfragmentandrecyclerview.adapter.ItemAdapter
-import com.example.activityfragmentandrecyclerview.data.Datasource
+import androidx.navigation.findNavController
+import com.example.activityfragmentandrecyclerview.databinding.FirstFragmentBinding
 
 class FirstFragment : Fragment() {
-
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.first_fragment, container, false)
+
+        //val binding: FirstFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.first_fragment, container, false)
+        //binding.btnToSecondFragment.setOnClickListener(
+        //        Navigation.createNavigateOnClickListener(R.id.action_firstFragment_to_secondFragment)
+        //)
+        val binding: FirstFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.first_fragment, container, false)
+        binding.btnToSecondFragment.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+        return binding.root
     }
 
-    override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(itemView, savedInstanceState)
-        val myDataset = Datasource().loadListItemModels()
-        val recyclerView = view!!.findViewById<RecyclerView>(R.id.recycler_view)
-        recyclerView.adapter = ItemAdapter(this, myDataset)
-        recyclerView.setHasFixedSize(true)
-    }
+    //override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
+        //super.onViewCreated(itemView, savedInstanceState)
+
+
+        //val myDataset = Datasource().loadListItemModels()
+        //val recyclerView = view!!.findViewById<RecyclerView>(R.id.recycler_view)
+        //recyclerView.adapter = ItemAdapter(requireContext(), myDataset)
+    //}
+
 }
